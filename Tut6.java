454a4addaba2d6feac24a4a02055725241ee1f5a
Elliot Parker <elliotccp@protonmail.com>
2020-09-13 20:05:36 +1000

    Add task 1 content

---
+++ Tut6.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Tut6 {
+    Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Tut6 tut = new Tut6();
+        int x = -1;
+        do {
+            x = tut.getNumber();
+        } while (x == -1);
+        System.out.print(x + " inputted");
+    }
+
+    public int getNumber() {
+        try {
+            System.out.println("Input integer: ");
+            int x = Integer.parseInt(input.nextLine());
+            return x;
+        } catch (NumberFormatException nfe) {
+            System.out.println("Invalid input");
+            return -1;
+        }
+    }
+}
